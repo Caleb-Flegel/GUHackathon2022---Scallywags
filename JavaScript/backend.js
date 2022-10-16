6ea7b788f2ce32c14de6cddf3cfe0de3161f3c2f
Jonathan Hamstra <jhamstra24@my.whitworth.edu>
2022-10-15 17:47:22 -0700

    Braindead.jpg

--- JavaScript/backend.js
+++ JavaScript/backend.js
@@ -1,10 +1,27 @@
 async function plswork() {
+    var xraw = prompt("Please enter X Coordinate")
+    var yraw = prompt("Please enter Y Coordinate")
+    xin = parseInt(xraw)
+    yin = parseInt(yraw)
+    
     const response = await fetch('/JavaScript/data.json');
-    const names = await response.json();
-    alert(names.squares[0].Image);
+    const entries = await response.json();
 
+    entrieslength = Object.keys(entries.squares).length
+    alert(entrieslength)
 
-    src = names.squares[0].Image,
+    var index = 0
+
+    for (let i = 0; i < entrieslength; i++) {
+        if (entries.squares[i].X_Coords == xin){
+            if (entries.squares[i].Y_Coords == yin){
+                index = i;
+            }}
+      }
+
+    alert(JSON.stringify(entries.squares[index]));
+
+    src = entries.squares[index].Image,
     img = document.createElement('img');
 
     img.src = src;
